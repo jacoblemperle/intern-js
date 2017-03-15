acf703a90139b8b1bd04c951b64a5536128bca86
Dennis Wilkins <dennis@inlineblock.com>
2017-03-15 15:02:53 -0600

    fixing tests

--- test/generators/generateTwitterSocialActivity.test.js
+++ test/generators/generateTwitterSocialActivity.test.js
@@ -7,7 +7,7 @@ describe('generateTwitterSocialActivity', () => {
     const activity = generateTwitterSocialActivity(TwitterFixture);
     expect(activity.ActivityURL).toEqual('http://twitter.com/MckeonLutz/statuses/751152691611463680');
     expect(activity.AuthorID).toEqual('id:twitter.com:1386906962');
-    expect(activity.AuthorPictureURL).toEqual('https://abs.twimg.com/sticky/default_profile_images/default_profile_0_normal.png"');
+    expect(activity.AuthorPictureURL).toEqual('https://abs.twimg.com/sticky/default_profile_images/default_profile_0_normal.png');
     expect(activity.AuthorRealName).toEqual('MckeonLutz');
     expect(activity.AuthorUsername).toEqual('MckeonLutz');
     expect(activity.Network).toEqual('twitter');
@@ -24,7 +24,7 @@ describe('generateTwitterSocialActivity', () => {
     expect(activity.AuthorUsername).toEqual('JenVento');
     expect(activity.Network).toEqual('twitter');
     expect(activity.ID).toEqual('tag:search.twitter.com,2005:794169597222064128');
-    expect(activity.BodyText).toEqual('RT @StylishRentals: Love this! \"Modern style Country Villa - Houses for Rent in Almoster\" @airbnb #Travelhttps://t.co/xJ479oYcx9');
+    expect(activity.BodyText).toEqual('How women could vote Hillary Clinton into the White House https://t.co/jnFqk44nr4 #imwithher');
   });
 
   xit('should returned undefined if missing id and body text', () => {
